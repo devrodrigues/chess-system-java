564b8c2a382fc7215851a89a27ee57eaa222a4da
Vinicius <vrf92@msn.com>
2024-07-12 20:46:58 -0300

    Imprimindo o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,4 +36,14 @@ public class Board {
 		this.pieces = pieces;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	
+	
 }
